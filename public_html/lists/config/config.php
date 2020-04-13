9c43349233f3e1c3d2654d77a17932ba97be76c1
Dustin Boling <dustin@dustinboling.com>
2020-04-12 22:24:11 -0700

    fixes location of .env

--- public_html/lists/config/config.php
+++ public_html/lists/config/config.php
@@ -1,7 +1,7 @@
 <?php
 // Loads variables from .env file
 require_once __DIR__ . '/../../../vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../../');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
 $dotenv->load();
 /*
 * ==============================================================================================================
